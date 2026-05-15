062bf8f52a9befeb96f9cd1963d063e773dd93aa
Your Name <youremail@example.com>
2026-05-15 14:51:28 -0400

    Added the 'SalesContract' class and extended it to the 'Contract' class. I made the constructor and added the super. Got all the getters/setters for each attribute, and implemented the 'getTotalPrice' and 'getMonthlyPayment' methods that was provided into the class.

---
+++ src/main/java/com/pluralsight/dealership/SalesContract.java
@@ -0,0 +1,75 @@
+package com.pluralsight.dealership;
+
+public class SalesContract extends Contract{
+    private double salesTaxAmount;
+    private int recordingFee;
+    private int processingFee;
+    private boolean financeOption;
+
+    public SalesContract(String date, String name, String email, Vehicle vehicleSold, double salesTaxAmount, int recordingFee, int processingFee, boolean financeOption) {
+        super(date, name, email, vehicleSold);
+        this.salesTaxAmount = salesTaxAmount;
+        this.recordingFee = recordingFee;
+        this.processingFee = processingFee;
+        this.financeOption = financeOption;
+    }
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+    public void setSalesTaxAmount(double salesTaxAmount) {
+        this.salesTaxAmount = salesTaxAmount;
+    }
+
+    public int getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(int recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public int getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(int processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinanceOption() {
+        return financeOption;
+    }
+
+    public void setFinanceOption(boolean financeOption) {
+        this.financeOption = financeOption;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return getVehicleSold().getPrice() + salesTaxAmount + recordingFee + processingFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 0;
+        double interestRate = 0;
+        if (financeOption) {
+            if (getVehicleSold().getPrice() >= 10000) {
+                numberOfPayments = 48;
+                interestRate = 4.25 / 1200;
+            } else {
+                numberOfPayments = 24;
+                interestRate = 5.25 / 1200;
+            }
+
+            double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+            monthlyPayment = Math.round(monthlyPayment * 100);
+            monthlyPayment /= 100;
+            return monthlyPayment;
+        } else {
+            return 0.0;
+        }
+    }
+}
